3199e42456947fa47d71eaf83ae67615fc49f697
Karai <lmanning17@gmail.com>
2011-02-02 21:51:55 -0400

    Initial Upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<script type="text/javascript" src="js/cookie.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/css.php"/>
+		<title>Time-based CSS v1.0</title>
+	</head>
+	<body>
+		<div id="timelapse"></div>
+	</body>
+</html>
